e2d8dfca0fb5f8ad4c5dd38dde46dd8f7165a652
matang <matang28@gmail.com>
2019-05-06 00:09:46 +0300

    - Adding the concept of a template engine - Creating impl to the template engine based on go's template engine

---
+++ data/go_template_engine.go
@@ -0,0 +1,30 @@
+package data
+
+import (
+	"bytes"
+	"text/template"
+)
+
+type GoTemplateEngine struct {
+}
+
+func NewGoTemplateEngine() *GoTemplateEngine {
+	return &GoTemplateEngine{}
+}
+
+func (this *GoTemplateEngine) Render(templateText string, data interface{}) (string, error) {
+	var out bytes.Buffer
+	t := template.New("template")
+
+	tree, err := t.Parse(templateText)
+	if err != nil {
+		return "", err
+	}
+
+	err = tree.Execute(&out, data)
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
+}
--- data/manifest.go
+++ data/manifest.go
@@ -12,3 +12,14 @@ type ConfigStore interface {
 	Put(key string, value interface{}) error
 	Get(key string, valueOut interface{}) bool
 }
+
+// Takes a template and a data structure
+// will expand the template based on the provided data
+type TemplateEngine interface {
+	Render(templateText string, data interface{}) (string, error)
+}
+
+// Will run a script file with the provided arguments
+type ScriptEngine interface {
+	Run(scriptFile string, args []string) error
+}
