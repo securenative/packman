22292da324491444f001d3d8d34b593c89e8ed3a
matang <matang28@gmail.com>
2019-05-07 08:56:21 +0300

    - Adding impl to the pack command - Adding impl to the unpack command

--- cmd/packman/controllers/pack.go
+++ cmd/packman/controllers/pack.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "gopkg.in/urfave/cli.v2"
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/urfave/cli.v2"
+)
 
 var PackCommand = cli.Command{
 	Name:      "pack",
@@ -8,6 +12,22 @@ var PackCommand = cli.Command{
 	Usage:     "pack <package-name> <path>",
 	UsageText: "Will pack the given folder and push it to the backend so it can be later located using the package-name",
 	Action: func(context *cli.Context) error {
-		return nil
+
+		if context.NArg() != 2 {
+			fmt.Println("pack expects exactly 2 arguments")
+		}
+
+		packageName := context.Args().Get(0)
+		path := context.Args().Get(1)
+
+		if packageName == "" {
+			return errors.New("you must provide a package name")
+		}
+
+		if path == "" {
+			return errors.New("you must provide a path to the project")
+		}
+
+		return PackmanModule.Packer.Pack(packageName, path)
 	},
 }
--- cmd/packman/controllers/unpack.go
+++ cmd/packman/controllers/unpack.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "gopkg.in/urfave/cli.v2"
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/urfave/cli.v2"
+)
 
 var UnpackCommand = cli.Command{
 	Name:      "unpack",
@@ -8,6 +12,31 @@ var UnpackCommand = cli.Command{
 	Usage:     "unpack <package-name> <dest-path>",
 	UsageText: "Will unpack the given package to the given destination path",
 	Action: func(context *cli.Context) error {
-		return nil
+
+		if context.NArg() != 2 {
+			fmt.Println("unpack expects exactly 2 argument")
+		}
+
+		packageName := context.Args().Get(0)
+		path := context.Args().Get(1)
+
+		if packageName == "" {
+			return errors.New("you must provide a package name")
+		}
+
+		if path == "" {
+			return errors.New("you must provide a path to the project")
+		}
+
+		return PackmanModule.Unpacker.Unpack(packageName, path, flagsArray(context))
 	},
+}
+
+func flagsArray(ctx *cli.Context) []string {
+	out := make([]string, ctx.NumFlags())
+	for _, flagName := range ctx.FlagNames() {
+		out = append(out, ctx.String(flagName))
+	}
+
+	return out
 }
