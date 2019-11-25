936e9492b72db252697d00049d0b9080647f715f
matang <matang28@gmail.com>
2019-11-25 21:05:34 +0200

    Adding release pipeline to github actions

--- internal/etc/args.go
+++ internal/etc/args.go
@@ -1,9 +1,17 @@
 package etc
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func ArgsToFlagsMap(args []string) map[string]string {
 	out := make(map[string]string)
+
+	if len(args)%2 != 0 {
+		panic(fmt.Errorf("num of flags must be an even number but got %d. flags must be k-v pairs such as -port 4000 -host 127.0.0.1", len(args)))
+	}
+
 	for idx, arg := range args {
 		if isFlagKey(arg) {
 			key := strings.TrimPrefix(arg, "-")
