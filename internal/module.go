7694d9950e4a46748000efca795e7be5d93b91cb
matang <matang28@gmail.com>
2019-11-25 16:46:20 +0200

    Adding release pipeline to github actions

--- internal/module.go
+++ internal/module.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
+	"errors"
 	"github.com/securenative/packman/internal/business"
 	"github.com/securenative/packman/internal/data"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 type Module struct {
@@ -20,7 +22,7 @@ type Module struct {
 var M *Module
 
 func init() {
-	home, err := os.UserHomeDir()
+	home, err := homeDir()
 	if err != nil {
 		panic(err)
 	}
@@ -48,4 +50,29 @@ func init() {
 		TemplatingService: business.NewTemplateService(remoteStorage, scriptEngine, templateEngine),
 		ConfigService:     business.NewConfigService(localStorage),
 	}
+}
+
+func homeDir() (string, error) {
+	env, enverr := "HOME", "$HOME"
+	switch runtime.GOOS {
+	case "windows":
+		env, enverr = "USERPROFILE", "%userprofile%"
+	case "plan9":
+		env, enverr = "home", "$home"
+	}
+	if v := os.Getenv(env); v != "" {
+		return v, nil
+	}
+	// On some geese the home directory is not always defined.
+	switch runtime.GOOS {
+	case "nacl":
+		return "/", nil
+	case "android":
+		return "/sdcard", nil
+	case "darwin":
+		if runtime.GOARCH == "arm" || runtime.GOARCH == "arm64" {
+			return "/", nil
+		}
+	}
+	return "", errors.New(enverr + " is not defined")
 }
