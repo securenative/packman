6bc167f33914f7720b3bf900d3bf2476f0957b59
matang28 <4709399+matang28@users.noreply.github.com>
2021-06-13 12:36:33 +0300

    Update template_service.go

--- internal/business/template_service.go
+++ internal/business/template_service.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	copy2 "github.com/otiai10/copy"
 	"github.com/securenative/packman/internal/data"
-	packman "github.com/securenative/packman/pkg"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
+
+const PackageName = "package_name"
+const PackagePath = "package_path"
 
 type templateService struct {
 	remoteStorage  data.RemoteStorage
@@ -29,8 +31,8 @@ func (this *templateService) Render(templatePath string, packagePath string, fla
 		}
 	}
 
-	flags[packman.PackagePathFlag] = packagePath
-	flags[packman.PackageNameFlag] = filepath.Base(packagePath)
+	flags[PackagePath] = packagePath
+	flags[PackageName] = filepath.Base(packagePath)
 
 	scriptPath, err := toScriptPath(packagePath)
 	if err != nil {
